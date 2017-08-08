9799aee5a006fb5159d943b863d8c4cfdb228144
RandomCas <prower_flash@163.com>
2017-08-08 22:08:33 +0800

    First commit

---
+++ app/src/main/java/com/example/coolweather/db/County.java
@@ -0,0 +1,49 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Try on 2017/8/8.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private int cityId;
+    private String weatherId;
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCountyName() {
+
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
